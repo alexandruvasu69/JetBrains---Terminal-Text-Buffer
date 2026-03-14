553f7030b1358b5b948dbdaee9b6635780b25a43
Alexandru Vasu <alexandru.vasu@gmail.com>
2026-03-14 14:31:30 +0100

    fix: change TerminalBuffer to implement interface and containarize dependenciese

--- src/main/java/com/example/TerminalBuffer.java
+++ src/main/java/com/example/TerminalBuffer.java
@@ -1,17 +1,19 @@
 package com.example;
 
-public class TerminalBuffer {
-    private int width;
-    private int height;
+public class TerminalBuffer implements ITerminalBuffer{
+    private IScreen screen;
+    private ICursor cursor;
     private int scrollBackMaxSize; 
+    private TerminalColor foregroundColor;
+    private TerminalColor backgroundColor;
+    private TextStyle textStyle;
 
     public TerminalBuffer(int width, int height, int scrollBackMaxSize) {
-        this.width = width;
-        this.height = height;
+        this.screen = new Screen(width, height);
         this.scrollBackMaxSize = scrollBackMaxSize;
         this.foregroundColor = TerminalColor.BLACK;
         this.backgroundColor = TerminalColor.BLACK;
-        this.cursor = new Cursor(width, height);
+        this.cursor = new Cursor(height - 2, width - 2);
     }
 
     public TerminalBuffer(int width, int height, int scrollBackMaxSize, ICursor cursor) {
@@ -19,36 +21,47 @@ public class TerminalBuffer {
         this.cursor = cursor;
     }
 
-    private TerminalColor foregroundColor;
-    private TerminalColor backgroundColor;
-    private TextStyle textStyle;
-    private ICursor cursor;
 
+    @Override
     public int getWidth() {
-        return width;
+        return screen.getWidth();
     }
 
+    @Override
     public int getHeight() {
-        return height;
+        return screen.getHeight();
     }
 
+    @Override
     public int getScrollBackMaxSize() {
         return scrollBackMaxSize;
     }
 
+    @Override
     public void setForegroundColor(TerminalColor foregroundColor) {
         this.foregroundColor = foregroundColor;
     }
 
+    @Override
     public void setBackgroundColor(TerminalColor backgroundColor) {
         this.backgroundColor = backgroundColor;
     }
 
+    @Override
     public void setTextStyle(TextStyle textStyle) {
         this.textStyle = textStyle;
     }
 
+    @Override
+    public void fillLine(char character) {
+        int row = cursor.getRowPosition();
+        screen.fillLine(character, row);
+    }
+
+    @Override
+    public String getScreenContent() {
+        return screen.getScreenString();
+    }
+
     
-
-
 }
