cc677adc0d917f512949e522a3f18bbc84dd9bcc
Alexandru Vasu <alexandru.vasu@gmail.com>
2026-03-14 13:12:50 +0100

    feature: add terminal color and text style enums

---
+++ src/main/java/com/example/TerminalColor.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public enum TerminalColor {
+    RED,
+    GREEN,
+    BLUE,
+    INDIGO,
+    ORANGE,
+    YELLOW,
+    VIOLET,
+    GREY,
+    MAROON,
+    BLACK,
+    OLIVE,
+    CYAN,
+    PINK,
+    MAGENTA,
+    TAN,
+    TEAL;
+}
