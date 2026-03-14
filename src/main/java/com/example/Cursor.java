6f8bbbf9776f4b4a6010371d9443a6a7ab61aa9b
Alexandru Vasu <alexandru.vasu@gmail.com>
2026-03-14 13:11:51 +0100

    feature: add cursor interface + impl

---
+++ src/main/java/com/example/Cursor.java
@@ -0,0 +1,52 @@
+package com.example;
+
+public class Cursor implements ICursor {
+    int rowPosition;
+    int columnPosition;
+
+    public Cursor(int rowPosition, int columnPosition) {
+        this.rowPosition = rowPosition;
+        this.columnPosition = columnPosition;
+    }
+
+    @Override
+    public int getRowPosition() {
+        return rowPosition;
+    }
+
+    @Override
+    public int getColumnPosition() {
+        return columnPosition;
+    }
+
+    @Override
+    public void setRowPosition(int rowPosition) {
+        this.rowPosition = rowPosition;
+    }
+
+    @Override
+    public void setColumnPosition(int columnPosition) {
+        this.columnPosition = columnPosition;
+    }
+
+    @Override
+    public void moveUp(int offset) {
+        this.rowPosition -= offset;
+    }
+
+    @Override
+    public void moveDown(int offset) {
+        this.rowPosition += offset;
+    }
+
+    @Override
+    public void moveLeft(int offset) {
+        this.columnPosition -= offset;
+    }
+
+    @Override
+    public void moveRight(int offset) {
+        this.columnPosition += offset;
+    }
+
+}
---
+++ src/main/java/com/example/ICursor.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public interface ICursor {
+    int getRowPosition();
+    int getColumnPosition();
+
+    void setRowPosition(int rowPosition);
+    void setColumnPosition(int columnPosition);
+
+    void moveUp(int offset);
+    void moveDown(int offset);
+    void moveLeft(int offset);
+    void moveRight(int offset);
+}
